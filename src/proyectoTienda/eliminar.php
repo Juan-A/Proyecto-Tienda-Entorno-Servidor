611287b5350dc32a212b7bea024f54d9a5574f1f
Juan Antonio Herrero Centurión <juan13herrero@gmail.com>
2023-11-13 19:24:06 +0100

    Implemented method for deleting products of the shopping cart.

--- src/proyectoTienda/eliminar.php
+++ src/proyectoTienda/eliminar.php
@@ -1,3 +1,13 @@
 <?
-
+require_once("inc/funciones/sesiones.php");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $cod = $_POST["codProd"];
+    $unidades = (int)$_POST["unidades"];
+    if($_SESSION["carrito"][$cod] > $unidades){
+        $_SESSION["carrito"][$cod] -= $unidades;
+    }else{
+        unset($_SESSION["carrito"][$cod]);
+    }
+}
+header("Location: carrito.php");
 ?>
