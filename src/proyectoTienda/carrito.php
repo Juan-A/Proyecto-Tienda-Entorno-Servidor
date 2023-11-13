babdaf02a4c738560f744144dfa258fa6e130de5
Juan Antonio Herrero Centurión <juan13herrero@gmail.com>
2023-11-13 18:27:45 +0100

    Created file "carrito" and implemented foreach to print the basket. (partially).

---
+++ src/proyectoTienda/carrito.php
@@ -0,0 +1,66 @@
+<?
+require_once("inc/bd.php");
+require_once("inc/funciones/sesiones.php");
+require_once("inc/funciones/productos.php");
+require_once("inc/secciones/cabecera.php");
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carrito</title>
+</head>
+<body>
+<?
+if(empty($_SESSION["carrito"])){
+    echo "<h2>Carrito vacío</h2>";
+}else{
+    $productos = cargar_productos(array_keys($_SESSION["carrito"]),$db);
+
+    ?>
+    <table>
+        <tr>
+            <th>Nombre</th>
+            <th>Descripción</th>
+            <th>Peso</th>
+            <th>Unidades</th>
+            <th>Eliminar</th>
+        </tr>
+    <?
+    foreach ($productos as $producto) {
+        ?>
+        <tr>
+            <td><?= $producto["nombre"] ?></td>
+            <td><?= $producto["descripcion"] ?></td>
+            <td><?= $producto["peso"] ?></td>
+            <td><?= $_SESSION["carrito"][$producto["codProd"]] ?></td>
+            <td>
+                <form action="eliminar.php" method="POST">
+                    <input type="hidden" value="<?=$producto["codProd"]?>" name="codProd">
+                    <button type="submit">Eliminar</button>
+                </form>
+            </td>
+        </tr>
+        <?
+    }
+    ?>
+
+    </table>
+
+
+
+
+
+
+    <?
+
+
+
+
+}
+?>    
+</body>
+</html>
