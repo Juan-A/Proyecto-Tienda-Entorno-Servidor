6d33cfe1560a945cc521afc289d42266fda3545b
Juan Antonio Herrero Centurión <juan13herrero@gmail.com>
2023-11-13 19:28:17 +0100

    Added menu links to another sections.

--- src/proyectoTienda/inc/secciones/cabecera.php
+++ src/proyectoTienda/inc/secciones/cabecera.php
@@ -1,6 +1,11 @@
 <header>
-    <div id="datosHeader">
-       <div>Usuario: <?= $_SESSION['usuario'] ?></div> 
-       <div>Nombre del restaurante: <?= $_SESSION["nombre"]?></div>
-    </div>
+    <nav>
+        <div id="datosHeader">
+            <div>Usuario: <?= $_SESSION['usuario'] ?></div>
+            <div>Nombre del restaurante: <?= $_SESSION["nombre"] ?></div>
+        </div>
+        <a href="categorias.php">Home</a>
+        <a href="carrito.php">Carrito</a>
+        <a href="cerrarSesion.php">Cerrar sesión</a>
+    </nav>
 </header>
