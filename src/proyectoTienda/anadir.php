5a952fd88fb7c339bf3082d8d9af1c2b4ddfe376
Juan Antonio Herrero Centurión <juan13herrero@gmail.com>
2023-11-13 17:57:08 +0100

    Fix if user doesn't come with a POST.

--- src/proyectoTienda/anadir.php
+++ src/proyectoTienda/anadir.php
@@ -1,10 +1,12 @@
 <?
 require_once("inc/funciones/sesiones.php");
-$cod = $_POST["codProd"];
-$unidades = (int)$_POST["unidades"];
-if(isset($_SESSION["carrito"][$cod])){
-    $_SESSION["carrito"][$cod] += $unidades;
-} else {
-    $_SESSION["carrito"][$cod] = $unidades;
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+    $cod = $_POST["codProd"];
+    $unidades = (int)$_POST["unidades"];
+    if (isset($_SESSION["carrito"][$cod])) {
+        $_SESSION["carrito"][$cod] += $unidades;
+    } else {
+        $_SESSION["carrito"][$cod] = $unidades;
+    }
 }
 header("Location: carrito.php");
