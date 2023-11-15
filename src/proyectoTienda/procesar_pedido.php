c551d7cdf99c7122fa3cc1b8942894c332bfef1f
Juan Antonio Herrero Centurión <juan13herrero@gmail.com>
2023-11-15 15:52:56 +0100

    Partialy implemented procesar_pedido

--- src/proyectoTienda/procesar_pedido.php
+++ src/proyectoTienda/procesar_pedido.php
@@ -1,4 +1,7 @@
 <?
-
+require_once("inc/funciones/sesiones.php");
+require_once("inc/funciones/pedidos.php");
+require_once("inc/bd.php");
+insertar_pedido($_SESSION["carrito"],$_SESSION["codRes"],$db);
 
 ?>
