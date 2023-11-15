1339ca9f6b793ceeb7d51ed1f56d8471959e7216
Juan Antonio Herrero Centurión <juan13herrero@gmail.com>
2023-11-15 16:30:27 +0100

    Corrected insert check.

--- src/proyectoTienda/procesar_pedido.php
+++ src/proyectoTienda/procesar_pedido.php
@@ -2,5 +2,7 @@
 require_once("inc/funciones/sesiones.php");
 require_once("inc/funciones/pedidos.php");
 require_once("inc/bd.php");
-echo insertar_pedido($_SESSION["carrito"],$_SESSION["codRes"],$db);
+if(!insertar_pedido($_SESSION["carrito"],$_SESSION["codRes"],$db)){
+    echo "Se ha producido un error al insertar el pedido.";
+}
 ?>
